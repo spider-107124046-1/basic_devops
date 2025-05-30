727c30abae060b0221ad5578024d3c1da0e9092c
Ilamparithi Murali <ilamparithi.in@gmail.com>
2025-05-30 21:53:34 +0530

    Checkpoint 1: get input folder arg and create files

--- vault_sweeper.sh
+++ vault_sweeper.sh
@@ -11,3 +11,19 @@ if [ ! -d "$INPUT_DIR" ]; then
     echo "Error: Directory '$INPUT_DIR' does not exist."
     exit 1
 fi
+
+# Step 2: Setup logs/ and maintainer
+LOG_DIR="$VAULT_DIR/logs"
+mkdir -p "$LOG_DIR"
+
+if ! id "maintainer" &>/dev/null; then
+    echo "Creating user 'maintainer'..."
+    sudo useradd -m maintainer
+else
+    echo "User 'maintainer' already exists."
+fi
+
+chown maintainer:maintainer "$LOG_DIR"
+chmod 700 "$LOG_DIR"
+touch "$LOG_DIR/metadata.log"
+
